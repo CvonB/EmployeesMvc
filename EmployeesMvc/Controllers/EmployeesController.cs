c4f465995c0d199deb70fbf53f8a960ad93e86b5
Christian von Bothmer <cvb.jaf@gmail.com>
2022-12-08 15:02:40 +0100

    Everything

---
+++ EmployeesMvc/Controllers/EmployeesController.cs
@@ -0,0 +1,44 @@
+﻿using EmployeesMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesMvc.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        EmployeeService service;
+
+        public EmployeesController()
+        {
+            service= new EmployeeService();
+        }
+
+        [HttpGet(""),HttpGet("index")]
+        public IActionResult Index()
+        {
+            return View(service.GetAll());
+        }
+
+        [HttpGet(nameof(Create))]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost(nameof(Create))]
+        public IActionResult Create(Employee employee)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            service.Add(employee);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet("details/{id}")]
+        public IActionResult Details(int id)
+        {
+            return View(service.GetById(id));
+        }
+    }
+}
---
+++ EmployeesMvc/Models/EmployeeService.cs
@@ -0,0 +1,28 @@
+﻿namespace EmployeesMvc.Models
+{
+    public class EmployeeService
+    {
+        static List<Employee> employees = new List<Employee>
+        {
+            new Employee{Id=1,Name="Eddy Binen",Email="Eddy@binen.com"},
+            new Employee{Id=2,Name="Christian von Bothmer",Email="Christian@von.bothmer"},
+            new Employee{Id=3,Name="Niklas Lindfors",Email="Niklas@lindfors.se"}
+        };
+
+        public void Add(Employee employee)
+        {
+            employee.Id = employees.Max(o => o.Id) + 1;
+            employees.Add(employee);
+        }
+
+        public Employee[] GetAll()
+        {
+            return employees.ToArray();
+        }
+
+        public Employee GetById(int id)
+        {
+            return employees.FirstOrDefault(o => o.Id == id);
+        }
+    }
+}
