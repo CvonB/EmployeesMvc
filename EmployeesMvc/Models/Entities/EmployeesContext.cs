26ecdcb48f593eb0ca7a7524d2e439c97bf27bc9
Christian von Bothmer <cvb.jaf@gmail.com>
2022-12-14 15:05:15 +0100

    stuff

--- EmployeesMvc/Models/Entities/EmployeesContext.cs
+++ EmployeesMvc/Models/Entities/EmployeesContext.cs
@@ -19,7 +19,7 @@ public partial class EmployeesContext : DbContext
         {
             entity.HasKey(e => e.Id).HasName("PK__Employee__3214EC074F2CE804");
 
-            entity.Property(e => e.Id).ValueGeneratedNever();
+
             entity.Property(e => e.Email).HasMaxLength(50);
             entity.Property(e => e.Name).HasMaxLength(50);
         });
